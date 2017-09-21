283c5013eb9ba87c9aa2173c8ae187c5acfed752
diego-asterisk <diegomanias@gmail.com>
2017-09-21 20:40:45 -0300

    ej 11 bm

--- ej11/test/bm.rb
+++ ej11/test/bm.rb
@@ -5,19 +5,19 @@ iterations = 100_000
 Benchmark.bm(27) do |bm|
   bm.report('rot_13_tr') do
     iterations.times do
-      rot_13_tr("¡Bienvenidos a la cursada 2015 de TTPS Opción Ruby!")
+      rot_13_tr("¡Bienvenidos a la cursada 2017 de TTPS Opción Ruby!")
     end
   end
 
   bm.report('rot_13_hash') do
     iterations.times do
-      rot_13_hash("¡Bienvenidos a la cursada 2015 de TTPS Opción Ruby!")
+      rot_13_hash("¡Bienvenidos a la cursada 2017 de TTPS Opción Ruby!")
     end
   end
 
   bm.report('rot_13_gsub') do
     iterations.times do
-      rot_13_tr("¡Bienvenidos a la cursada 2015 de TTPS Opción Ruby!")
+      rot_13_tr("¡Bienvenidos a la cursada 2017 de TTPS Opción Ruby!")
     end
   end
 end
